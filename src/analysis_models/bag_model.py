fb85774887bda22a75cc51e2846571bcc4f87c4c
Chrisaxell <31619201+Chrisaxell@users.noreply.github.com>
2020-10-08 17:38:48 +0200

    Default values på data_manager.py initialisert bag_model.py oppdatert .gitignore

--- src/analysis_models/bag_model.py
+++ src/analysis_models/bag_model.py
@@ -1,4 +1,5 @@
 from src.data_manager import DataManager
+import pandas
 
 
 class BagModel:
@@ -7,4 +8,11 @@ class BagModel:
         self.dm = dm
 
     def get_missing_bags(self):
+        self.dm.read_data_files()
+        df = self.dm.bag_messages
+        print(df)
         pass
+
+
+hello = BagModel(DataManager())
+hello.get_missing_bags()
